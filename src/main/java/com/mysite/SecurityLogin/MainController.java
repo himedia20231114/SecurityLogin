e19459503d11f9e564247f260a66db2ce7c82698
Jangwoo Park <p.jangwoo@gmail>
2024-01-29 10:27:58 +0900

    1월 29일 - 아침

---
+++ src/main/java/com/mysite/SecurityLogin/MainController.java
@@ -0,0 +1,16 @@
+package com.mysite.SecurityLogin;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+	// http://localhost:8787
+	@GetMapping("/")
+	public String main() {
+		
+		return "index" ;
+	}
+
+}
